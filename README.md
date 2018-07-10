0af43bbe8202285742ede327aa8bd133d20cf9e5
Maksym Kononenko <maksym.kononenko@synapse.com>
2018-07-10 19:38:19 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# tf-azure-swarm
+# tf-azure-lab
+
+### AZURE CLI FOR cygwin
+https://gist.github.com/avoidik/2ae9257240065a2b6733cc4f0c0e993f
+### LAB URL
+https://docs.microsoft.com/en-us/azure/terraform/terraform-create-vm-cluster-with-infrastructure
+
+read -sp "Azure password: " AZ_PASS && echo && az.cmd login -u kononenkomi@gmail.com -p $AZ_PASS
+az account show --query "{subscriptionId:id, tenantId:tenantId}"
+Next, create separate credentials for Terraform.
+az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/${SUBSCRIPTION_ID}"
+az vm show --resource-group myResourceGroup --name myVM -d --query [publicIps] --o tsv
+
+*Create "terraform.tfvars" and put your credentials here like:*
+```
+client_id       = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+client_secret   = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+tenant_id       = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+subscription_id = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+```
---
+++ test.tf
@@ -0,0 +1,122 @@
+provider "azurerm" {
+    client_id       = "${var.client_id}"
+    client_secret   = "${var.client_secret}"
+    tenant_id       = "${var.tenant_id}"
+    subscription_id = "${var.subscription_id}"
+}
+
+resource "azurerm_resource_group" "myterraformgroup" {
+    name     = "myResourceGroup"
+    location = "eastus"
+
+    tags {
+        environment = "Terraform Demo"
+    }
+}
+
+resource "azurerm_virtual_network" "myterraformnetwork" {
+    name                = "myVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "eastus"
+    resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
+
+    tags {
+        environment = "Terraform Demo"
+    }
+}
+
+resource "azurerm_subnet" "myterraformsubnet" {
+    name                 = "mySubnet"
+    resource_group_name  = "${azurerm_resource_group.myterraformgroup.name}"
+    virtual_network_name = "${azurerm_virtual_network.myterraformnetwork.name}"
+    address_prefix       = "10.0.2.0/24"
+}
+
+resource "azurerm_public_ip" "myterraformpublicip" {
+    name                         = "myPublicIP"
+    location                     = "eastus"
+    resource_group_name          = "${azurerm_resource_group.myterraformgroup.name}"
+    public_ip_address_allocation = "dynamic"
+
+    tags {
+        environment = "Terraform Demo"
+    }
+}
+
+resource "azurerm_network_security_group" "myterraformnsg" {
+    name                = "myNetworkSecurityGroup"
+    location            = "eastus"
+    resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags {
+        environment = "Terraform Demo"
+    }
+}
+
+resource "azurerm_network_interface" "myterraformnic" {
+    name                = "myNIC"
+    location            = "eastus"
+    resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
+
+    ip_configuration {
+        name                          = "myNicConfiguration"
+        subnet_id                     = "${azurerm_subnet.myterraformsubnet.id}"
+        private_ip_address_allocation = "dynamic"
+        public_ip_address_id          = "${azurerm_public_ip.myterraformpublicip.id}"
+    }
+
+    tags {
+        environment = "Terraform Demo"
+    }
+}
+
+resource "azurerm_virtual_machine" "myterraformvm" {
+    name                  = "myVM"
+    location              = "eastus"
+    resource_group_name   = "${azurerm_resource_group.myterraformgroup.name}"
+    network_interface_ids = ["${azurerm_network_interface.myterraformnic.id}"]
+    vm_size               = "Standard_DS1_v2"
+
+    storage_os_disk {
+        name              = "myOsDisk"
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = "Premium_LRS"
+    }
+
+    storage_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "16.04.0-LTS"
+        version   = "latest"
+    }
+
+    os_profile {
+        computer_name  = "myvm"
+        admin_username = "azureuser"
+    }
+
+    os_profile_linux_config {
+        disable_password_authentication = true
+        ssh_keys {
+            path     = "/home/azureuser/.ssh/authorized_keys"
+            key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDLPVtOQwHxGFtqlsFoiA2UsFTX+wC7MBJnGlw2O4jsDyJj0SkI7BOCa4TpF9W3I/zEeFl3iok0WkLlXMKrE6XzDWaXqAV3ST0OXZs1iNWEBWoYnltSp54uRMCYqHcgrwyAeuHbAnJDE3BQ5pHX4KP/HHcqsLYl4Si9RWAACm7fwdhF5H7wOKtJV9ixmA3rjwkh9Yeqy/kWFYbqtlAJ19KDSqbtqUCMWCNPeoXPIH0h5EXW3hH/RSw70K4Xrds0qUMwcOM06aC5dbY2VODM4QPBqmrjN0rEzRoghUSuIy63eQnuQlJkpOSXCBK4DSK0QhMpG6N20OLKMU5OcvO7ets7"
+        }
+    }
+
+    tags {
+        environment = "Terraform Demo"
+    }
+}
